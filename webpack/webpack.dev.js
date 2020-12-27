d4bcef51638ad61b39d5e787d5d195abeb6d05fb
WesselSmit <45405413+WesselSmit@users.noreply.github.com>
2020-12-27 17:39:14 +0100

    Create webpack.dev.js

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,39 @@
+const path = require('path')
+
+module.exports = {
+    entry: path.join(__dirname, '/src/scripts/index.ts'), // points to your apps entry file(s)
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    
+    // use to watch for changes
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    
+    module: {
+        rules: [
+            { // typescript support
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src/scripts')]
+            },
+            
+            // styling files are imported in webpack's entry file(s)
+            { // less support
+                test: /\.less$/, 
+                use: [ 
+                    'style-loader',
+                    'css-loader', 
+                    'less-loader'
+                ],
+                include: [path.resolve(__dirname, 'src/styles')]
+            }
+        ]
+    },
+    plugins: [],
+    devtool: 'source-map'
+}
